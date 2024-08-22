4ef0d8c4b4f9ae393d37622a9f8917dc1acf41cd
Mahmoud <mahmoudradwan@Mahmouds-MacBook-Pro.local>
2024-08-22 18:27:36 -0400

    optimized program and added sentiment analysis feature and front-end

--- transcribe.py
+++ transcribe.py
@@ -20,52 +20,116 @@ import configparser
 import json
 import threading
 import time
+import sys
 
 import pyaudio
 import websocket
 from websocket._abnf import ABNF
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtCore import QThread, pyqtSignal
+import openai
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
-# Even if your default input is multi channel (like a webcam mic),
-# it's really important to only record 1 channel, as the STT service
-# does not do anything useful with stereo. You get a lot of "hmmm"
-# back.
 CHANNELS = 1
-# Rate is important, nothing works without it. This is a pretty
-# standard default. If you have an audio device that requires
-# something different, change this.
 RATE = 44100
 RECORD_SECONDS = 5
 FINALS = []
 LAST = None
 
 REGION_MAP = {
-    'us-east': 'gateway-wdc.watsonplatform.net',
+    'us-east': 'api.us-east.speech-to-text.watson.cloud.ibm.com/instances/170112cf-ba4e-4e08-953d-3bb8ad6faa97',
     'us-south': 'stream.watsonplatform.net',
     'eu-gb': 'stream.watsonplatform.net',
     'eu-de': 'stream-fra.watsonplatform.net',
     'au-syd': 'gateway-syd.watsonplatform.net',
     'jp-tok': 'gateway-syd.watsonplatform.net',
 }
+openai.api_key = 'your open-ai key'
+class SpeechThread(QThread):
+    new_message = pyqtSignal(str)
+    finished = pyqtSignal(str)
+    sentiment_result = pyqtSignal(str)  # Signal to emit sentiment results
 
+    def __init__(self, timeout):
+        super().__init__()
+        self.timeout = timeout
+        self.transcript_chunk = ""  # Store the transcript for analysis
+
+    def run(self):
+        ws = websocket.WebSocketApp(url,
+                                    header=headers,
+                                    on_message=self.on_message,
+                                    on_error=self.on_error,
+                                    on_close=self.on_close)
+        ws.on_open = lambda ws: self.on_open(ws)
+        ws.run_forever()
+
+    def on_message(self, ws, msg):
+        """Handle incoming messages."""
+        global LAST
+        data = json.loads(msg)
+        if "results" in data:
+            partial_transcript = data['results'][0]['alternatives'][0]['transcript'].strip()
+            
+            if data["results"][0]["final"]:
+                FINALS.append(data)
+                LAST = None
+                self.transcript_chunk += " " + partial_transcript
+                self.transcript_chunk = self.transcript_chunk.strip()
+
+                # Emit the full transcript
+                transcript = "".join([x['results'][0]['alternatives'][0]['transcript']
+                                      for x in FINALS])
+                self.new_message.emit(transcript)
+
+                # Analyze the sentiment of the entire chunk
+                self.analyze_sentiment(self.transcript_chunk)
+            else:
+                LAST = data
+                self.new_message.emit(self.transcript_chunk + " " + partial_transcript)
+
+    def on_error(self, ws, error):
+        """Handle errors."""
+        print(error)
+
+    def on_close(self, ws, close_status_code, close_msg):
+        """Handle the closing of the WebSocket connection."""
+        global LAST
+        if LAST:
+            FINALS.append(LAST)
+        transcript = "".join([x['results'][0]['alternatives'][0]['transcript']
+                              for x in FINALS])
+        self.finished.emit(transcript)
+
+    def on_open(self, ws):
+        """Handle WebSocket opening."""
+        data = {
+            "action": "start",
+            "content-type": "audio/l16;rate=%d" % RATE,
+            "continuous": True,
+            "interim_results": True,
+            "word_confidence": True,
+            "timestamps": True,
+            "max_alternatives": 3
+        }
+        ws.send(json.dumps(data).encode('utf8'))
+        threading.Thread(target=read_audio, args=(ws, self.timeout)).start()
+
+    def analyze_sentiment(self, text):
+        """Send the text to OpenAI for sentiment analysis."""
+        response = openai.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "system", "content": "Analyze the sentiment of the following text and respond with one of three answers: NEUTRAL, NEGATIVE, POSITIVE."},
+                      {"role": "user", "content": text}]
+        )
+        sentiment = response.choices[0].message.content.strip()
+        self.sentiment_result.emit(sentiment)
 
 def read_audio(ws, timeout):
-    """Read audio and sent it to the websocket port.
-
-    This uses pyaudio to read from a device in chunks and send these
-    over the websocket wire.
-
-    """
+    """Read audio and send it to the WebSocket."""
     global RATE
     p = pyaudio.PyAudio()
-    # NOTE(sdague): if you don't seem to be getting anything off of
-    # this you might need to specify:
-    #
-    #    input_device_index=N,
-    #
-    # Where N is an int. You'll need to do a dump of your input
-    # devices to figure out which one you want.
     RATE = int(p.get_default_input_device_info()['defaultSampleRate'])
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
@@ -78,10 +142,6 @@ def read_audio(ws, timeout):
 
     for i in range(0, int(RATE / CHUNK * rec)):
         data = stream.read(CHUNK)
-        # print("Sending packet... %d" % i)
-        # NOTE(sdague): we're sending raw binary in the stream, we
-        # need to indicate that otherwise the stream service
-        # interprets this as text control messages.
         ws.send(data, ABNF.OPCODE_BINARY)
 
     # Disconnect the audio stream
@@ -89,90 +149,22 @@ def read_audio(ws, timeout):
     stream.close()
     print("* done recording")
 
-    # In order to get a final response from STT we send a stop, this
-    # will force a final=True return message.
+    # Send stop message to get the final response from STT
     data = {"action": "stop"}
     ws.send(json.dumps(data).encode('utf8'))
-    # ... which we need to wait for before we shutdown the websocket
     time.sleep(1)
     ws.close()
 
-    # ... and kill the audio device
+    # Terminate the audio device
     p.terminate()
-
-
-def on_message(self, msg):
-    """Print whatever messages come in.
-
-    While we are processing any non trivial stream of speech Watson
-    will start chunking results into bits of transcripts that it
-    considers "final", and start on a new stretch. It's not always
-    clear why it does this. However, it means that as we are
-    processing text, any time we see a final chunk, we need to save it
-    off for later.
-    """
-    global LAST
-    data = json.loads(msg)
-    if "results" in data:
-        if data["results"][0]["final"]:
-            FINALS.append(data)
-            LAST = None
-        else:
-            LAST = data
-        # This prints out the current fragment that we are working on
-        print(data['results'][0]['alternatives'][0]['transcript'])
-
-
-def on_error(self, error):
-    """Print any errors."""
-    print(error)
-
-
-def on_close(ws):
-    """Upon close, print the complete and final transcript."""
-    global LAST
-    if LAST:
-        FINALS.append(LAST)
-    transcript = "".join([x['results'][0]['alternatives'][0]['transcript']
-                          for x in FINALS])
-    print(transcript)
-
-
-def on_open(ws):
-    """Triggered as soon a we have an active connection."""
-    args = ws.args
-    data = {
-        "action": "start",
-        # this means we get to send it straight raw sampling
-        "content-type": "audio/l16;rate=%d" % RATE,
-        "continuous": True,
-        "interim_results": True,
-        # "inactivity_timeout": 5, # in order to use this effectively
-        # you need other tests to handle what happens if the socket is
-        # closed by the server.
-        "word_confidence": True,
-        "timestamps": True,
-        "max_alternatives": 3
-    }
-
-    # Send the initial control message which sets expectations for the
-    # binary stream that follows:
-    ws.send(json.dumps(data).encode('utf8'))
-    # Spin off a dedicated thread where we are going to read and
-    # stream out audio.
-    threading.Thread(target=read_audio,
-                     args=(ws, args.timeout)).start()
 
 def get_url():
     config = configparser.RawConfigParser()
     config.read('speech.cfg')
-    # See
-    # https://console.bluemix.net/docs/services/speech-to-text/websockets.html#websockets
-    # for details on which endpoints are for each region.
     region = config.get('auth', 'region')
     host = REGION_MAP[region]
-    return ("wss://{}/speech-to-text/api/v1/recognize"
-           "?model=en-US_BroadbandModel").format(host)
+    return ("wss://{}/v1/recognize"
+           "?model=en-AU_BroadbandModel").format(host)
 
 def get_auth():
     config = configparser.RawConfigParser()
@@ -180,43 +172,55 @@ def get_auth():
     apikey = config.get('auth', 'apikey')
     return ("apikey", apikey)
 
-
 def parse_args():
-    parser = argparse.ArgumentParser(
-        description='Transcribe Watson text in real time')
+    parser = argparse.ArgumentParser(description='Transcribe Watson text in real time')
     parser.add_argument('-t', '--timeout', type=int, default=5)
-    # parser.add_argument('-d', '--device')
-    # parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
     return args
 
+def create_window():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    window.setWindowTitle("Watson Speech to Text with Sentiment Analysis")
+    window.setGeometry(100, 100, 800, 600)
 
-def main():
-    # Connect to websocket interfaces
+    widget = QWidget()
+    layout = QVBoxLayout()
+
+    text_edit = QTextEdit()
+    text_edit.setReadOnly(True)
+
+    sentiment_label = QLabel("Sentiment: ")
+    layout.addWidget(text_edit)
+    layout.addWidget(sentiment_label)
+
+    widget.setLayout(layout)
+    window.setCentralWidget(widget)
+
+    def update_text(message):
+        text_edit.setText(message)  # Replace the entire text
+
+    def update_sentiment(sentiment):
+        sentiment_label.setText("Sentiment: " + sentiment)  # Update sentiment label
+
+    def update_transcript(transcript):
+        text_edit.setText("\nClosing transcript:\n" + transcript)  # Replace the entire text
+
+    args = parse_args()
+    global url, headers
     headers = {}
     userpass = ":".join(get_auth())
-    headers["Authorization"] = "Basic " + base64.b64encode(
-        userpass.encode()).decode()
+    headers["Authorization"] = "Basic " + base64.b64encode(userpass.encode()).decode()
     url = get_url()
 
-    # If you really want to see everything going across the wire,
-    # uncomment this. However realize the trace is going to also do
-    # things like dump the binary sound packets in text in the
-    # console.
-    #
-    # websocket.enableTrace(True)
-    ws = websocket.WebSocketApp(url,
-                                header=headers,
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
-    ws.on_open = on_open
-    ws.args = parse_args()
-    # This gives control over the WebSocketApp. This is a blocking
-    # call, so it won't return until the ws.close() gets called (after
-    # 6 seconds in the dedicated thread).
-    ws.run_forever()
+    speech_thread = SpeechThread(args.timeout)
+    speech_thread.new_message.connect(update_text)
+    speech_thread.finished.connect(update_transcript)
+    speech_thread.sentiment_result.connect(update_sentiment)
+    speech_thread.start()
 
+    window.show()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
-    main()
+    create_window()
